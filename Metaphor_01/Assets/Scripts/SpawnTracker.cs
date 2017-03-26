90afafec85bb2e5b388f237618788fd18c4fc1f3
Ike Ellsworth <isaac@kina.net>
2017-03-26 15:59:10 -0600

    Set up object spawners to respawn either on death or on a timer, based on flags.

---
+++ Metaphor_01/Assets/Scripts/SpawnTracker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTracker : MonoBehaviour
+{
+    private static SpawnTracker _singleton;
+
+    public List<ObjectSpawner> Objects = new List<ObjectSpawner>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        _singleton = this;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // An ObjectSpawner reports here and gets added to the list.
+    public static void ReportSpawner(ObjectSpawner reportee)
+    {
+        _singleton.Objects.Add(reportee);
+    }
+
+    public static void TriggerReset()
+    {
+        foreach (var spawner in _singleton.Objects)
+        {
+            spawner.FlagForRespawnOnDeath();
+        }
+    }
+}
