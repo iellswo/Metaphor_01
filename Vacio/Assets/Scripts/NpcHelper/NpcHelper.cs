6b6fcf8c16ec0ab8e524f3dc2a73aecb9671122b
Thomas Keene <eneekmot@yahoo.com>
2017-08-23 16:18:20 -0600

    * Helper NPC follows player with simple MoveTowards.

---
+++ Vacio/Assets/Scripts/NpcHelper/NpcHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcHelper : MonoBehaviour
+{
+    private enum EHelperState
+    {
+        
+    }
+    private PlayerController playerTarget = null;
+
+    private void Start()
+    {
+        playerTarget = GameObject.FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (playerTarget != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerTarget.transform.position, Time.deltaTime);
+        }
+        else
+        {
+            Debug.LogError("Could not find playerTarget", gameObject);
+        }
+    }
+}
