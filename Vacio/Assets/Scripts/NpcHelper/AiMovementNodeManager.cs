319811919338e33cbbe7e358e622009392e972c0
Thomas Keene <eneekmot@yahoo.com>
2017-08-23 17:41:56 -0600

    * Added component for AI movement nodes * AI movement nodes draw colored lines between each other to indicate the type of node they are.

---
+++ Vacio/Assets/Scripts/NpcHelper/AiMovementNodeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiMovementNodeManager : MonoBehaviour
+{
+    public enum EConnectionType
+    {
+        Walking,
+    }
+
+    // This could be static, but making it a component should make it easier to get scene events and clean it up when loading a new level.
+    private static AiMovementNodeManager singleton = null;
+    public static bool TryGetInstance(out AiMovementNodeManager manager)
+    {
+        if (singleton == null)
+        {
+            GameObject singletonGameObject = new GameObject();
+            singleton = singletonGameObject.AddComponent<AiMovementNodeManager>();
+            singletonGameObject.name = "AiMovementNodeManager";
+        }
+        manager = singleton;
+        return true;
+    }
+}
