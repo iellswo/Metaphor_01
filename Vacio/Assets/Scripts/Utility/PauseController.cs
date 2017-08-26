52b6925178261d2fc2d942a73622a5b494845e86
Ike Ellsworth <isaac@kina.net>
2017-08-26 16:17:11 -0600

    Created a Pause menu and added it to each level scene.

---
+++ Vacio/Assets/Scripts/Utility/PauseController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public Canvas MenuCanvas;
+    public PlayerController Controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        UnpauseGame();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetButtonDown("Pause") && Time.timeScale == 1)
+        {
+            PauseGame();
+        }
+        else if ((Input.GetButtonDown("Pause") || Input.GetButtonDown("Cancel")) && Time.timeScale == 0)
+        {
+            UnpauseGame();
+        }
+    }
+
+    #region Button Commands
+    public void ContinuePressed()
+    {
+        UnpauseGame();
+    }
+    #endregion
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        MenuCanvas.gameObject.SetActive(true);
+        Controller.enabled = false;
+    }
+
+    private void UnpauseGame()
+    {
+        Time.timeScale = 1;
+        MenuCanvas.gameObject.SetActive(false);
+        Controller.enabled = true;
+    }
+}
