d7350fb9f8686afef2689425af6dbe608a0824d8
Ike Ellsworth <isaac@kina.net>
2017-08-27 00:37:00 -0600

    Set up temporary credits scene.

---
+++ Vacio/Assets/Scripts/Utility/TempCreditsController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class TempCreditsController : MonoBehaviour
+{
+    public void OnButton_Return()
+    {
+        Initiate.Fade("Main Menu", Color.black, 1f);
+    }
+}
